36e9afb573b7096022dde9e4447b05d7aff43c33
Rolando Abarca <m@rolando.cl>
2025-07-20 12:02:28 -0700

    wip

---
+++ router.scm
@@ -0,0 +1,86 @@
+(import
+ spiffy
+ format
+ intarweb
+ uri-common
+ srfi-13
+ srfi-18)
+
+(define (schematra-default-handler)
+  (lambda (#!optional params)
+    "Welcome to Schematra"))
+
+(define (make-path-tree)
+  '("/" schematra-default-handler))
+
+;; find a resource based on a path on a tree. Returns the handler if
+;; found, #f otherwise.
+;; TODO: implement a version of this. Assume path is a list with strings and tree is a tree-like list, where each node can be compared to a path element. The leaf nodes should have a `procedure` that can be returned. If nothing is found, then it should return #f
+(define (find-resource path tree)
+  )
+
+;; empty routes. each verb has a list of routes
+(define schematra-get-routes '())
+(define schematra-post-routes '())
+(define schematra-vhost-default ".*")
+
+(define (get path body)
+  (let ((path-list (uri-path (uri-reference path))))
+    (hash-table-set! schematra-get-routes path-list body)))
+
+(define (post path body)
+  (let ((path-list (uri-path (uri-reference path))))
+    (hash-table-set! schematra-post-routes path-list body)))
+
+(define (alist? x)
+  (and (list? x)
+       (every pair? x)))
+
+(define (is-response? list)
+  (and
+   ;; is this a list?
+   (list? list)
+   ;; we need a pair of at least two items: status & body
+   (>= (length list) 2)
+   ;; first element should be a valid status symbol
+   (symbol? (car list))
+   ;; next item should be a valid body-type
+   (string? (cadr list))
+   ;; should check for headers next
+   (if (= 3 (length list)) (alist? (list-ref list 2)) #t)))
+
+(define (schematra-parse-response resp)
+  (cond
+   [(string? resp) (send-response status: 'ok body: resp)]
+   [else
+    (send-response
+     status: 'error
+     body: (format #f "Error: response type not supported (~A)" resp))]))
+
+(define (schematra-request-augment request)
+  request)
+
+(define (schematra-router continue)
+  (let* ((request (current-request))
+	 (method (request-method request))
+	 (path (uri-path (request-uri request)))
+	 (route-handlers
+	  (cond
+	   [(eq? method 'GET) schematra-get-routes]
+	   [(eq? method 'POST) schematra-post-routes]
+	   [else (error "no handlers for this method")]))
+	 (handler (hash-table-ref/default route-handlers path #f)))
+    (format #t "Req: ~A. Path: ~A. Method: ~A\n" request path method)
+    (if handler (schematra-parse-response (handler request)) (continue))))
+
+;; install vhost handler
+(vhost-map
+ `((,schematra-vhost-default . ,(lambda (continue) (schematra-router continue)))))
+
+(get "/foo/:bar"
+     (lambda (request #!optional (params '()))
+       "OK"))
+
+(thread-start!
+ (lambda ()
+   (start-server port: 8080)))
