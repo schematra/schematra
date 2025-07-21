51f2aea5b79a10e335b1957e9f8ddff28474b36c
Rolando Abarca <m@rolando.cl>
2025-07-20 17:48:11 -0700

    add example that showcases url params

--- demo.scm
+++ demo.scm
@@ -1,9 +1,16 @@
 (load "schematra.scm")
-(import schematra)
+(import schematra format)
 
 (get "/"
      (lambda (request #!optional params)
        "welcome to schematra"))
+
+(get "/users/:user-id/posts/:post-id"
+     (lambda (req params)
+       (let ((user-id (alist-ref "user-id" params equal?))
+             (post-id (alist-ref "post-id" params equal?)))
+         (format "User: ~A, Post: ~A\n" user-id post-id))))
+
 
 (schematra-install)
 (schematra-start development?: #t)
