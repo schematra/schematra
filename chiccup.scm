a51e73848bddc4a82e42999ab7f85f7d96d8c6fb
Rolando Abarca <m@rolando.cl>
2025-08-17 20:56:17 -0700

    migrating chiccup to use sxml-transforms

--- chiccup.scm
+++ chiccup.scm
@@ -19,30 +19,30 @@
 (module
  chiccup
  (
-  ccup/html
+  ccup->html
   ccup/doctype
+  ccup->sxml
+  ccup/raw-content-tags
   ) ;; end export list
 
  (import scheme)
- (import format
-	 srfi-1
+ (import srfi-1
 	 srfi-13
 	 chicken.base
-	 chicken.string)
-
- ;; https://developer.mozilla.org/en-US/docs/Glossary/Void_element
- (define void-elements
-   '(area base br col embed hr img input link meta source track wbr))
+	 chicken.string
+	 chicken.port
+	 chicken.pretty-print
+	 sxml-transforms)
 
  ;; Parse a CSS-style selector string or symbol into parts (tag, classes, id)
  ;;
  ;; ## Examples
  ;; ```scheme
- ;; (parse-selector "div")     => ("div" () #f)
- ;; (parse-selector 'div.cls)  => ("div" ("cls") #f)
- ;; (parse-selector ".cls")    => ("div" ("cls") #f)
- ;; (parse-selector "div#id")  => ("div" () "id")
- ;; (parse-selector "#id")     => ("div" () "id")
+ ;; (parse-selector "div")     => (div () #f)
+ ;; (parse-selector 'div.cls)  => (div ("cls") #f)
+ ;; (parse-selector ".cls")    => (div ("cls") #f)
+ ;; (parse-selector "div#id")  => (div () "id")
+ ;; (parse-selector "#id")     => (div () "id")
  ;; ```
  ;;
  ;; **Note:** Following CSS conventions, ID (#id) must come last in the selector.
@@ -67,161 +67,144 @@
 		     (else first-part))))
      (list (string->symbol (string-downcase tag-name)) class-list id-str)))
 
- ;; Convert a parsed selector specification into an attribute alist.
- ;;
- ;; ## Parameters
- ;; `SPEC-LIST` should be a list of the form `(tag classes id)` where:
- ;; - `tag` is a symbol representing the HTML tag name
- ;; - `classes` is a list of strings representing CSS classes
- ;; - `id` is a string or `#f` representing the element ID
- ;;
- ;; ## Returns
- ;; An alist of `(attr-name . attr-value)` pairs suitable for HTML element
- ;; attributes. Empty when given invalid input.
- ;;
- ;; ## Example
- ;; ```scheme
- ;; '(div ("btn" "primary") "submit") => '(("class" . "btn primary") ("id" . "submit"))
- ;; ```
- (define (build-attrs spec-list)
+ ;; Convert a parsed selector specification into SXML attributes
+ (define (build-sxml-attrs spec-list)
    (if (= (length spec-list) 3)
        (let ((classes (cadr spec-list))
 	     (id (caddr spec-list)))
 	 (append
 	  (if (and classes
-		   (list? classes) ;; ensure classes is a list
+		   (list? classes)
 		   (not (null? classes)))
-	      `(("class" . ,(string-join classes " ")))
+	      `((class ,(string-join classes " ")))
 	      '())
-	  (if id `(("id" . ,id)) '())))
+	  (if id `((id ,id)) '())))
        '()))
 
- (define (merge-attrs spec-attrs explicit-attrs)
-   (let* ((spec-class (assoc "class" spec-attrs))
-          (explicit-class (assoc "class" explicit-attrs))
+ ;; Merge selector attributes with explicit @ attributes
+ (define (merge-sxml-attrs spec-attrs explicit-attrs)
+   (let* ((spec-class (assoc 'class spec-attrs))
+          (explicit-class (assoc 'class explicit-attrs))
           (merged-class-attr
            (cond
             ((and spec-class explicit-class)
-             `(("class" . ,(string-append (cdr spec-class) " " (cdr explicit-class)))))
+             `((class ,(string-append (cadr spec-class) " " (cadr explicit-class)))))
             (spec-class `(,spec-class))
             (explicit-class `(,explicit-class))
             (else '())))
-          (non-class-spec (filter (lambda (attr) (not (string=? (car attr) "class"))) spec-attrs))
-          (non-class-explicit (filter (lambda (attr) (not (string=? (car attr) "class"))) explicit-attrs))
+          (non-class-spec (filter (lambda (attr) (not (eq? (car attr) 'class))) spec-attrs))
+          (non-class-explicit (filter (lambda (attr) (not (eq? (car attr) 'class))) explicit-attrs))
 	  (explicit-keys (map car non-class-explicit))
 	  (spec-pruned
-	   (filter (lambda (attr) (not (member (car attr) explicit-keys string=?)))
+	   (filter (lambda (attr) (not (member (car attr) explicit-keys)))
 		   non-class-spec)))
      (append merged-class-attr spec-pruned non-class-explicit)))
 
- ;; copied from Spiffy
- (define (htmlize str)
-   (string-translate* (format "~A" str)
-		      '(("&" . "&amp;") ("<" . "&lt;") (">" . "&gt;") ("\"" . "&quot;") ("'" . "&#x27;"))))
+ ;; Convert chiccup element to SXML
+ (define (ccup->sxml element-spec)
+   (cond
+    ;; Handle raw content - mark with special tag for processing
+    ((and (list? element-spec) (eq? (car element-spec) 'raw))
+     `(*RAW* ,(cadr element-spec)))
+    ;; Handle regular chiccup elements
+    ((list? element-spec)
+     (let* ((parsed (parse-selector (car element-spec)))
+	    (tag (car parsed))
+	    (spec-attrs (build-sxml-attrs parsed))
+	    (rest-parts (cdr element-spec))
+	    ;; Check if second element is @ attributes
+	    (has-at-attrs? (and (not (null? rest-parts))
+				(list? (car rest-parts))
+				(not (null? (car rest-parts)))
+				(eq? (caar rest-parts) '@)))
+	    (explicit-attrs (if has-at-attrs? (cdar rest-parts) '()))
+	    (merged-attrs (merge-sxml-attrs spec-attrs explicit-attrs))
+	    (body-parts (if has-at-attrs? (cdr rest-parts) rest-parts))
+	    (sxml-body (map ccup->sxml body-parts)))
+       (if (null? merged-attrs)
+	   `(,tag ,@sxml-body)
+	   `(,tag (@ ,@merged-attrs) ,@sxml-body))))
+    ;; Handle strings and other content
+    (else element-spec)))
 
  (define ccup/doctype (make-parameter "<!doctype html>"))
+ (define ccup/raw-content-tags (make-parameter '(script style)))
 
- (define (attr-key->string k)
-   (cond ((string? k) k)
-	 ((symbol? k) (string-translate (string-downcase (symbol->string k)) "_" "-"))
-	 (else (format "~A" k))))
+ ;; Custom entag that removes the leading newline and handles text escaping
+ (define (ccup/entag tag elems)
+   (cond
+    ;; For *RAW* tags, return the structure as-is for later processing
+    ((eq? tag '*RAW*)
+     (list tag (car elems)))
+    ;; Regular case: generate HTML tags, escape based on raw-content-tags list
+    (else
+     (if (and (pair? elems) (pair? (car elems)) (eq? '@ (caar elems)))
+         (list #\< tag (cdar elems) #\>
+               (and (pair? (cdr elems))
+                    (list (cdr elems) "</" tag #\>)))
+         (list #\< tag #\> (and (pair? elems)
+                                (list elems "</" tag #\>)))))))
 
- (define (normalize-attrs alist)
-   (map (lambda (p) (cons (attr-key->string (car p)) (cdr p))) alist))
-
- (define (render-attr attr)
-   (let ((k (car attr))
-	 (v (cdr attr)))
+ ;; Custom SXML->HTML without newlines and with raw content support
+ (define (ccup/SXML->HTML tree)
+   (define (unwrap-raw content)
      (cond
-      ((eq? v #t) (attr-key->string k)) ;; boolean attribute
-      ((or (eq? v #f) (not v)) #f) ;; omit false/nil
-      (else (string-append (attr-key->string k)
-			   "=\"" (htmlize v) "\"")))))
+      ((and (list? content) 
+            (not (null? content)) 
+            (eq? (car content) '*RAW*))
+       (cadr content))  ; Unwrap raw content
+      ((and (list? content) (not (null? content)))
+       (map unwrap-raw content))  ; Recursively process non-empty lists
+      (else content)))  ; Pass through everything else (strings, chars, etc.)
+   
+   (let ((processed-tree (pre-post-order* tree
+                                         `((@
+                                            ((*default* . ,(lambda (attr-key value) (enattr attr-key value))))
+                                            . ,(lambda (trigger value) (cons '@ value)))
+                                           (*default* . ,(lambda (tag elems)
+                                                           (ccup/entag tag elems)))
+                                           (*text* . ,(lambda (trigger str) str))))))
+     (SRV:send-reply (unwrap-raw processed-tree))))
 
-;;; Generate an HTML tag from an element-spec list.
-;;;
-;;; ### Parameters
-;;; `ELEMENT-SPEC` is a list with at least one item that defines a
-;;; CSS-inspired selector. If the tag name is not specified it's assumed
-;;; to be a `div`. The second element can be an alist with the extra
-;;; attributes and the last element is expected to be the body, which can
-;;; be nested element-specs.
-;;;
-;;; ### Security
-;;; String content is automatically HTML-sanitized to prevent XSS attacks.
-;;; To inject raw HTML without sanitization, wrap it with `(raw "content")`.
-;;;
-;;; ### Examples
-;;; ```scheme
-;;; (ccup/html `[.h-4.w-4 "content"])
-;;; ;; => <div class="h-4 w-4">content</div>
-;;;
-;;; (ccup/html `[div "< & >"])
-;;; ;; => <div>&lt; &amp; &gt;</div>
-;;;
-;;; (ccup/html `[div (raw "<em>italic</em>")])
-;;; ;; => <div><em>italic</em></div>
-;;;
-;;; (ccup/html
-;;;   `[ul#foo ({ hx-post . "/my/endpoint"})
-;;;     [li "some item"]])
-;;; ;; => <ul id="foo" hx-post="/my/endpoint"><li>some item</li></ul>
-;;;
-;;; (ccup/html
-;;;   `[button ((disabled . #t)) "the button"])
-;;; ;; => <button disabled>the button</button>
-;;; ```
- (define (ccup/html element-spec)
-   (let* ( ;; parse the selector
-	  (parsed (parse-selector (car element-spec)))
-	  (tag (car parsed))
-	  (spec-attrs (build-attrs parsed))
-	  ;; detect whether the caller passed an explicit alist as second element
-	  (second-arg (if (> (length element-spec) 1) (cadr element-spec) '()))
-	  (has-explicit-attrs?
-	   (and (list? second-arg)
-		(not (null? second-arg))
-		(pair? (car second-arg))))
-	  ;; merge the two possible attribute lists
-	  (explicit-attrs* (if has-explicit-attrs? (normalize-attrs second-arg) '()))
-	  (merged-attrs (merge-attrs spec-attrs explicit-attrs*))
-	  ;; collect the body parts, which can be a string or another
-	  ;; element-spec
-	  (body-parts
-	   (if has-explicit-attrs? 
-	       (cddr element-spec) ; skip selector and attrs, take rest as body
-	       (cdr element-spec))) ; skip selector, take rest as body
-	  ;; build the final attribute string
-	  (rendered-attrs (filter identity (map render-attr merged-attrs)))
-	  (attr-str (if (null? rendered-attrs)
-			""
-			(string-append " " (string-join rendered-attrs))))
-	  ;; build the body
-	  (body-str (if (null? body-parts)
-			""
-			(apply string-append
-			       (map (lambda (part)
-				      (cond
-				       ;; sanitize string
-				       ((string? part) (htmlize part))
-				       ;; skip empty parts
-				       ((null? part) "")
-				       ;; allow raw strings [tag ('raw "<em>text</em>")]
-				       ((and (list? part) (eq? (car part) 'raw)) (cadr part))
-				       ;; assume a list might be another element spec
-				       ((list? part) (ccup/html part))
-				       ;; anything else, just try its string representation
-				       (else (format "~A" part))))
-				    body-parts)))))
-     ;; finally render
-     (if (member tag void-elements)
-	 (begin
-	   (when (not (null? body-parts))
-	     (error (format "Void element '~a' cannot have body content: ~a" tag body-parts)))
-	   (string-append "<" (symbol->string tag) attr-str ">"))
-	 (let ((tag-str (symbol->string tag)))
-	   (if (eq? tag 'html)
-	       (string-append (ccup/doctype) "<html" attr-str ">" body-str "</html>")
-	       (string-append "<" tag-str attr-str ">" body-str "</" tag-str ">"))))))
+ ;; Generate HTML from a chiccup element-spec by converting to SXML first.
+ ;;
+ ;; ### Parameters
+ ;; `ELEMENT-SPEC` is a list with at least one item that defines a
+ ;; CSS-inspired selector. If the tag name is not specified it's assumed
+ ;; to be a `div`. The second element can optionally be `(@ ...)` with SXML
+ ;; attributes, and the remaining elements are the body content.
+ ;;
+ ;; ### Security
+ ;; String content is automatically HTML-sanitized to prevent XSS attacks.
+ ;; To inject raw HTML without sanitization, wrap it with `(raw "content")`.
+ ;;
+ ;; ### Examples
+ ;; ```scheme
+ ;; (ccup/html `[.h-4.w-4 "content"])
+ ;; ;; => <div class="h-4 w-4">content</div>
+ ;;
+ ;; (ccup/html `[div "< & >"])
+ ;; ;; => <div>&lt; &amp; &gt;</div>
+ ;;
+ ;; (ccup/html `[div (raw "<em>italic</em>")])
+ ;; ;; => <div><em>italic</em></div>
+ ;;
+ ;; (ccup/html `[button (@ (disabled) (type "submit")) "Click me"])
+ ;; ;; => <button disabled type="submit">Click me</button>
+ ;;
+ ;; (ccup/html `[ul#foo (@ (hx-post "/endpoint")) [li "item"]])
+ ;; ;; => <ul id="foo" hx-post="/endpoint"><li>item</li></ul>
+ ;; ```
+ (define (ccup->html element-spec)
+   (let* ((sxml-tree (ccup->sxml element-spec))
+	  (html-content (with-output-to-string
+			  (lambda ()
+			    (ccup/SXML->HTML sxml-tree)))))
+     (if (eq? (car sxml-tree) 'html)
+	 ;; Special handling for html tag to include doctype
+	 (string-append (ccup/doctype) html-content)
+	 ;; Regular element
+	 html-content)))
+
  ;; end module
  )
